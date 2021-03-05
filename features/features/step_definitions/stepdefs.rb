a926598dffca6b11002db639b5cd6d9f867dac64
Keisuke Nakama <keisuke.nakama@2nd-community.com>
2021-03-05 09:31:51 +0900

    japanese version

--- features/features/step_definitions/stepdefs.rb
+++ features/features/step_definitions/stepdefs.rb
@@ -6,6 +6,14 @@ module FridayStepHelper
       'Nope'
     end
   end
+
+  def is_it_friday_ja(day)
+    if day == '金曜'
+      '華金！'
+    else
+      'ちがうよ'
+    end
+  end
 end
 World FridayStepHelper
 
@@ -18,5 +26,18 @@ When("I ask whether it's Friday yet") do
 end
 
 Then("I should be told {string}") do |expected_answer|
+  expect(@actual_answer).to eq (expected_answer)
+end
+
+# 日本語
+Given("今日が {string}") do |given_day|
+  @today = given_day
+end
+
+When("金曜と訪ねたら") do
+  @actual_answer = is_it_friday_ja(@today)
+end
+
+Then("{string}と返答する") do |expected_answer|
   expect(@actual_answer).to eq (expected_answer)
 end
